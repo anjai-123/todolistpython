a68d73186c210c0f990d86804710aa7a8d21dcdb
anjali-123 <anjalichandrantp@gmail.com>
2022-05-04 19:38:25 -0700

    added

---
+++ todolistproject/todolistapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TodoForm
+
+# Create your views here.
+def home(request):
+    task1 = Task.objects.all()
+    if request.method=='POST':
+        name=request.POST.get('task','')
+        priority=request.POST.get('priority','')
+        date=request.POST.get('date','')
+        task=Task(name=name,priority=priority,date=date)
+        task.save()
+    return render(request,'home.html',{'task1':task1})
+def delete(request,task_id):
+    task=Task.objects.get(id=task_id)
+    if request.method=='POST':
+       task.delete()
+       return redirect('/')
+    return render(request,'delete.html')
+def update(request,id):
+    task=Task.objects.get(id=id)
+    form=TodoForm(request.POST or None,instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('/')
+    return render(request,'edit.html',{'form':form,'task':task})
